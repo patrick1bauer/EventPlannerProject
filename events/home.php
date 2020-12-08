870a375ee7d263c58ceb3e9a959f9138a70476fd
patrick1bauer <patrick1bauer@gmail.com>
2020-12-08 18:06:58 -0500

    Update home.php

--- events/home.php
+++ events/home.php
@@ -1,33 +1,22 @@
 <?php
-// Initialize the session
-// session_start();
-
-include '../helper.php';
-/*
-// Check if the user is logged in, if not then redirect them to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
-{
-	header("location: ../login.php");
-	exit;
-}
-*/
+include('../helper.php');
 
 print <<<EOF
 <html>
   <body>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    EOF;
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+EOF;
 
-    // navbar();
+navbar();
 
-    print <<<EOF
+print <<<EOF
     <div class="container">
       <div class="jumbotron">
       <h1>EventPlannerProject</h1>
@@ -37,4 +26,5 @@ print <<<EOF
   <body>
 </html>
 EOF;
+
 ?>
