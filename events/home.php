9f0d47d9b23bb4da804dce3d8b9d2ba84313cb12
patrick1bauer <patrick1bauer@gmail.com>
2020-12-08 18:00:54 -0500

    Update home.php

--- events/home.php
+++ events/home.php
@@ -1,16 +1,16 @@
 <?php
 // Initialize the session
-session_start();
+// session_start();
 
 include '../helper.php';
-
+/*
 // Check if the user is logged in, if not then redirect them to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 {
 	header("location: ../login.php");
 	exit;
 }
-
+*/
 
 print <<<EOF
 <html>
