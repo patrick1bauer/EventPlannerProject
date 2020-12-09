d2dbc58dc0e1285ecdb9412e1b94729b28547617
Samuel <armi.sam99@gmail.com>
2020-12-08 22:50:15 -0500

    + Updated confirm create event screen

--- events/createEvent.php
+++ events/createEvent.php
@@ -1,6 +1,20 @@
 <?php
-// Loads database config
-include('../connection.php');
+// Close connection
+$conn = null;
+// Initialize the session
+session_start();
+
+include('../events/listEvents.php');
+include('../helper.php');
+
+// Check if the user is logged in, if not then redirect them to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+{
+	header("location: http://www.eventplannerproject.com/EventPlannerProject/login.php");
+	exit;
+}
+
+navbar();
 
 $name = $_POST['name'];
 $admin = $_POST['admin'];
@@ -25,6 +39,13 @@ $insertEvent->execute();
 // Get the new ID of the events table entry
 $eid = $conn->lastInsertId();
 
+
+// Get user's credentials
+$arr = json_decode(stripslashes($_COOKIE['login']), true);
+$myid = $arr['uid'];
+$name = $arr['name'];
+$super_admin = $arr['super_admin'];
+
 // Check if insertEvent was successful
 if(!$insertEvent)
 	echo('Error inserting event!');
@@ -37,9 +58,63 @@ else
 	if(!$insertEventRequest)
 		echo('Error requesting to join event!');
 	else
-		echo("Successfully requested to create event!<br><button onclick=\"location.href='../interfaces/adminInterface.php'\" type=\"button\">Back to Admin Interface</button>");
+	{
+		
+		echo("
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Search Events</title>
+				<style>
+					table,td
+					{
+						border: 1px solid grey;
+					}
+						th{
+					background-color: grey;
+					color: white;
+					}
+					tr:nth-child(even){
+					background-color: #e0e0e0;
+					color: black;
+					}
+			</style>
+				<meta charset='UTF-8'>
+				<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css'>
+				<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+				<style type='text/css'>
+					body{ font: 14px sans-serif; }
+					.wrapper{ width: 350px; padding: 20px; }
+				</style>
+			</head>
+			<body>
+				<div class='container'>
+					<div class='jumbotron'>
+						<h1 style='fontSize=21;'>Success, your event is in!</h1>
+						<p>Return to the Admin Interface to continue.
+						</p>
+					</div>
+					
+					<div class='container'>
+						<p>
+							<div>
+							<?php echo resetPassword() ?>
+							<?php echo signOut() ?>
+							</div>
+						</p>
+					</div>
+				</div>
+				<div class='container'>
+					<button class='btn btn-primary mb-2' onclick=\"location.href='../interfaces/adminInterface.php'\" type='button'>Back to Admin Interface</button>
+				</div>
+			</body	
+		</html>");
+	
+	
+
+	}
+	
+	//"<br><button onclick=\"location.href='../interfaces/adminInterface.php'\" type=\"button\">Back to Admin Interface</button>");
 }
 
-// Close connection
-$conn = null;
 ?>
